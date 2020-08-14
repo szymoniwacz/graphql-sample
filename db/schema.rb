81814379097aceb4bef4a8a28fbeb14c8a1c33ee
Szymon Iwacz <szymon@iwacz.pl>
2020-08-14 19:07:36 +0200

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_145907) do
+ActiveRecord::Schema.define(version: 2020_08_14_170554) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
