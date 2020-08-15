531a884ad9feec6e5c5414f82ff55ba3db22a89e
Szymon Iwacz <szymon@iwacz.pl>
2020-08-15 08:42:12 +0200

    Add user authentication

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -9,19 +9,36 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      session: session,
+      current_user: current_user
     }
-    result = GraphqlSampleSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = GraphqlSampleSchema.execute(
+      query,
+      variables: variables,
+      context: context,
+      operation_name: operation_name
+    )
     render json: result
-  rescue => e
+  rescue StandrdError => e
     raise e unless Rails.env.development?
+
     handle_error_in_development e
   end
 
   private
 
-  # Handle form data, JSON body, or a blank value
+  def current_user
+    # if we want to change the sign-in strategy, this is the place to do it
+    return unless session[:token]
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+    token = crypt.decrypt_and_verify session[:token]
+    user_id = token.gsub('user-id:', '').to_i
+    User.find user_id
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
+
   def ensure_hash(ambiguous_param)
     case ambiguous_param
     when String
