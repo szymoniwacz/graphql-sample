4a080c99e9045cbe9f8ceaab043d6778a9e0691c
Szymon Iwacz <szymon@iwacz.pl>
2020-08-15 10:01:30 +0200

    Add create vote mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_link, mutation: Mutations::CreateLink
     field :create_user, mutation: Mutations::CreateUser
     field :sign_in_user, mutation: Mutations::SignInUser
+    field :create_vote, mutation: Mutations::CreateVote
   end
 end
